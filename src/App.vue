b8134d6e346ebca6de84b783cbbbb774a0a0ff19
buddha <buddhatamang1000@gmail.com>
2021-11-24 10:29:48 +0545

    feat:add link and style navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="min-h-screen min-w-full bg-gray-200">
-    <div class="mx-auto pt-10 w-4/6 items-center">
+    <div class="mx-auto pt-4 pb-10 w-4/6 items-center">
+      <NavBar />
       <router-view />
     </div>
   </div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
 
 <style lang="scss"></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="
+      flex
+      items-center
+      justify-center
+      mb-8
+      text-2xl text-gray-500
+      space-x-8
+    "
+  >
+    <router-link :to="{ name: 'Home' }" class="hover:text-gray-800"
+      >Projects</router-link
+    >
+    <router-link :to="{ name: 'AddProject' }" class="hover:text-gray-800"
+      >Add New Project</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
